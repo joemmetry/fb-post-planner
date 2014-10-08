26452602a61f68cb93ffe66cba4eae6a0dd37946
joemscore27 <josephemmanuel.petilla@pup.edu.ph>
2014-10-08 10:27:07 +0800

    Phase 2
    Modified Schedule Post Template
    *Added time format checker
    *Added datepicker
    Added Server Processes
    *Convert Short lived token to Long lived token
    *Save post details to database

--- main.py
+++ main.py
@@ -1,23 +1,129 @@
 import os
 import urllib
 import datetime
-
-from google.appengine.api import users
-from google.appengine.ext import ndb
-
+import json
 import jinja2
 import webapp2
 import logging
+import sched
+import time
+import threading
 
+from google.appengine.api import users
+from google.appengine.api import urlfetch
+from google.appengine.ext import ndb
+from google.appengine.ext import db
+from datetime import datetime, timedelta
+
+#VARIABLES
+FB_AID = "704513729631955"
+FB_ASE = "c6cf3ccc279e225f90da48129ed5ea04"
+FB_API_URI = "https://graph.facebook.com/v2.1"
+FB_OAUTH_URI = "https://graph.facebook.com/oauth/access_token"
 JINJA_ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def contentFetchFromApp(a):
+    data = {
+        "method" : "post",
+        "message" : a.content,
+        "access_token" : a.accessToken
+    }
+    return data
+
+def contentPostToFacebook(a,b):
+    #a is data b is id
+    form_data = urllib.urlencode(a)
+    url = FB_API_URI + "/" + b + "/feed"
+    result = urlfetch.fetch(
+        url=url,
+        payload=form_data,
+        method=urlfetch.POST
+    )
+    content = json.loads(result.content)
+    return content
+
+def facebookGetCurrentToken(a):
+    b = a.split("&")[0].split("=")[1]
+    return {
+        "access_token" : b
+    }
+
+def facebookTokenConvert(a,self):
+    data = {
+        "grant_type" : "fb_exchange_token",
+        "fb_exchange_token" : a,
+        "client_id" : FB_AID,
+        "client_secret" : FB_ASE
+    }
+    form_data = urllib.urlencode(data)
+    result = urlfetch.fetch(
+        url =FB_OAUTH_URI,
+        payload = form_data,
+        method = urlfetch.POST
+    )
+    converted = facebookGetCurrentToken(result.content)
+    return converted
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    	page = JINJA_ENV.get_template('pages/index.html')
-        self.response.write(page.render())
+        fetchContents = SPContents.query().fetch()
+        getUser = self.request.cookies.get('socA')
+        getID = self.request.cookies.get('socC')
+        values = {
+            'fetched': fetchContents
+        }
+        page = JINJA_ENV.get_template('pages/index.html')
+        self.response.write(page.render(values))
+
+class SaveHandler(webapp2.RequestHandler):
+    def post(self):
+        saveContent = SPContents()
+        saveContent.userFullName = self.request.get('fbName')
+        saveContent.userID = self.request.get('fbID')
+
+        getToken = self.request.get('fbAccessToken')
+        longToken = facebookTokenConvert(getToken, self)
+        saveContent.accessToken = longToken["access_token"]
+
+        saveContent.content = self.request.get('fbMsg')
+        saveContent.postAudience = self.request.get('fbAudience')
+
+        formatDate = self.request.get('fbDate') + " " + self.request.get('fbClock')
+        saveContent.postDatePrinted = formatDate
+        saveContent.postDate = datetime.strptime(formatDate, "%m-%d-%Y %I:%M %p")
+
+        saveContent.postExpires = self.request.get('fbMsgExpire')
+        saveContent.put()
+
+class PostScheduledHandler(webapp2.RequestHandler):
+    def get(self):
+        a = SPContents()
+        a.postDate = datetime.now()
+        b = SPContents.query(
+                ndb.AND(SPContents.postDate <= datetime.now() + timedelta(hours=8),
+                    SPContents.postStatus=="Pending")
+            ).fetch()
+        for c in b:
+            data = contentFetchFromApp(c)
+            contentPostToFacebook(data, c.userID)
+            c.postStatus = "Completed"
+            c.put()
+
+#Field Fetchers
+class SPContents(ndb.Model):
+    post_date = ndb.DateTimeProperty(auto_now_add=True)
+    userFullName = ndb.StringProperty(indexed=False)
+    userID = ndb.StringProperty(indexed=False)
+    accessToken = ndb.StringProperty(indexed=False)
+    content = ndb.StringProperty(indexed=False)
+    postDate = ndb.DateTimeProperty()
+    postDatePrinted = ndb.StringProperty(indexed=False)
+    postExpires = ndb.StringProperty(indexed=False)
+    postStatus = ndb.StringProperty(default="Pending")
+    postAudience = ndb.StringProperty(default="1")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
--- static/js/postplanner.js
+++ static/js/postplanner.js
@@ -1,272 +1,291 @@
-"use strict";
-/*your code here*/
-$(document).ready(function(){
-	/*$.graphModal({
-		type:'message',
-		theme:'dark',
-		title: 'Facebook Post Planner',
-		content: 'Welcome to Facebook Post Planner&mdash;Test I. Under construction. Wait. Wait. Wait.'
-	});*/
-	PostPlanner.FB.init();
-    $('#fbPost').click(function(){ /*click the post button*/
-    	var a = $('#fbMsg').val(); //fetch the value of message
-    	console.log(a);
-    	var b = {
-    		method: "post",
-    		message: a,
-    		access_token:currentToken
-    	};
-    	$.get('https://graph.facebook.com/v2.1/me/feed',b)
-    		.done(function(){
-    			$.graphModal({
-		    		type:'message',
-		    		theme:'dark',
-		    		title: 'Facebook Post',
-		    		content: 'Your have posted successfully.'
-		    	});
-		    	$('#fbMsg').val('');
-    		})
-    		.fail(function(){
-    			$.graphModal({
-		    		type:'message',
-		    		theme:'dark',
-		    		title: 'Facebook Post',
-		    		content: 'An error occurred. Please try again.'
-		    	});	
-    		});
-    	
-    });
-
-});
-
-var PostPlanner = {}, /*create global namespace*/
-	currentID = function(){return (Cookie.get('socA')!="" ? Cookie.get('socA') : "0");}, 
-	currentToken = function(){return (Cookie.get('socB')!="" ? Cookie.get('socB') : "0");},
-	currentInfo = function(){return (Cookie.get('socC')!="" ? Cookie.get('socC') : "");};
-	
-
-/*
- *	FB sub-namespace for Facebook
- *	Chainable except the connect object
- */
-PostPlanner.FB = {
-	init: function(){
-		this.connect(this.apiID);
-	},
-	apiID: "704513729631955",
-	connect: function(a){
-		window.fbAsyncInit = function() {
-        FB.init({
-          appId      : a,
-          xfbml      : true,
-          version    : 'v2.1'
+'use strict';
+$(document).ready(function () {
+    PostPlanner.FB.init();
+    $('#fbPost').click(function () {
+        var a = $('#fbMsg').val();
+        console.log(a);
+        var b = {
+            method: 'post',
+            message: a,
+            access_token: currentToken
+        };
+        $.get('https://graph.facebook.com/v2.1/me/feed', b).done(function () {
+            $.graphModal({
+                type: 'message',
+                theme: 'dark',
+                title: 'Facebook Post',
+                content: 'Your have posted successfully.'
+            });
+            $('#fbMsg').val('');
+        }).fail(function () {
+            $.graphModal({
+                type: 'message',
+                theme: 'dark',
+                title: 'Facebook Post',
+                content: 'An error occurred. Please try again.'
+            });
         });
-		if(typeof FB != "undefined"){
-			/*check fb user status and refresh access token*/
-			FB.getLoginStatus(function(zz){
-				if(zz.status == "connected" && zz.authResponse.userID == Cookie.get('socA')){
-					PostPlanner.FB.info({
-		            	a: zz.authResponse.userID,
-		            	b: zz.authResponse.accessToken
-		            });
-				}
-				PostPlanner.FB.pic();
-			});
-			/*assign function to login link*/
-      		implementFunctions(); 
-		}
-      };
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "//connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-	},
-	pic: 	function(){
-		FB.api(
-		    "/me/picture",
-			{
-		        "redirect": false,
-		        "height": "50",
-		        "type": "normal",
-		        "width": "50"
-		    },
-		    function (response) {
-		      if (response && !response.error){Cookie.set('socD',response.data.url);}
-		      else{Cookie.set('socD','/static/img/user-40.png');}
+    });
+});
+var PostPlanner = {},
+    currentID = function () {
+        return (Cookie.get('socA') != '' ? Cookie.get('socA') : '0');
+    },
+    currentToken = function () {
+        return (Cookie.get('socB') != '' ? Cookie.get('socB') : '0');
+    },
+    currentInfo = function () {
+        return (Cookie.get('socC') != '' ? Cookie.get('socC') : '');
+    };
+PostPlanner.FB = {
+    init: function () {
+        this.connect(this.apiID);
+    },
+    apiID: '704513729631955',
+    connect: function (a) {
+        window.fbAsyncInit = function () {
+            FB.init({
+                appId: a,
+                xfbml: true,
+                version: 'v2.1'
+            });
+            if (typeof FB != 'undefined') {
+                FB.getLoginStatus(	function (response) {
+                    if (response.status == 'connected' && response.authResponse.userID == Cookie.get('socA')) {
+                        PostPlanner.FB.info({
+                            a: response.authResponse.userID,
+                            b: response.authResponse.accessToken
+                        }).pic().checkLogin(response);
+                    }
+                });
+                implementFunctions();
+            }
+        };
+        (function (d, _0xbec7x9, _0xbec7xa) {
+            var _0xbec7xb, _0xbec7xc = d.getElementsByTagName(_0xbec7x9)[0];
+            if (d.getElementById(_0xbec7xa)) {
+                return;
+            }
+            _0xbec7xb = d.createElement(_0xbec7x9);
+            _0xbec7xb.id= _0xbec7xa;
+            _0xbec7xb.src= '//connect.facebook.net/en_US/sdk.js';
+            _0xbec7xc.parentNode.insertBefore(_0xbec7xb, _0xbec7xc);
+        }(document, 'script', 'facebook-jssdk'));
+    },
+    pic: function () {
+        FB.api('/me/picture', {
+            "\x72\x65\x64\x69\x72\x65\x63\x74": false,
+            "\x68\x65\x69\x67\x68\x74": '50',
+            "\x74\x79\x70\x65": 'normal',
+            "\x77\x69\x64\x74\x68": '50'
+        }, function (a) {
+            if (a && !a.error) {
+                Cookie.set('socD', a.data.url);
+            } else {
+                Cookie.set('socD', '/static/img/user-40.png');
+            }
+            $('.fb-names img').prop('src', Cookie.get('socD'));
+        });
+        return this;
+    },
+    info: function (response) {
+        currentID = response.a;
+        currentToken = response.b;
+        Cookie.set('socA', currentID);
+        Cookie.set('socB', currentToken);
+        $('[name="fbID"]').val(Cookie.get('socA'));
+        $('[name="fbAccessToken"]').val(Cookie.get('socB'));
+        $('[name="fbName"]').val(Cookie.get('socC'));
+        return this;
+    },
+    me: function (a) {
+        if (typeof a == 'function') {
+            FB.api('/me', a);
+        } else {
+            console.warn('Ooops. This object only accepts functions.');
+        }
+        return this;
+    },
+    checkLogin: function(a,b){
+        console.log(a)
+        if(!a || typeof a == "undefined"){ 
+            console.warn('Ooops. There is no object here. Try again.');return this;
+        }
 
-			  $('.fb-names img').prop('src',Cookie.get('socD') );
-		    }
-		);	
+
+		if (Cookie.get('socA') != '' && Cookie.get('socA') == a.authResponse.userID &&
+                                    typeof b == "boolean" && b==true ) {
+		    FB.logout();
+		    this.isLoggedOut(true);
+		    return false;
+		}
+		this.info({
+		    a: a.authResponse.userID,
+		    b: a.authResponse.accessToken
+		}).me(
+                                        function (a) {
+                                            if (a && !a.error) {
+                                                Cookie.set('socC', a.name);
+                                                Cookie.set('socE', a.link);
+                                                $('[name="fbName"]').val(a.name);
+                                                $('.fb-names span a').html(Cookie.get('socC'));
+                                                $('.fb-names span a').attr('target', '_blank');
+                                                $('.fb-names span a').attr('href', Cookie.get('socE'));
+                                            }
+		}).isLoggedOut(false);
 		return this;
-	},
-	info: 	function(zz){
-		currentID = zz.a;
-		currentToken = zz.b;
-		Cookie.set('socA', currentID);
-	    Cookie.set('socB', currentToken);
-	    return this;
-	},
-	me: 	function(a){
-		if(typeof a == "function"){
-			FB.api(
-			    "/me",
-			    a
-			);
-		}
-		else{
-			console.alert('Ooops. This object only accepts functions.');
-		}
-		return this;
-	},
-	isLoggedOut: function(a){
-		if(typeof a == "boolean"){
-			if(a === true){
-		  		FB.logout();
-				currentID=currentToken="0";
-				this.info({
-		            	a: '',
-		            	b: ''
-		        });
-	            Cookie.set('socC','Guest');
-	            Cookie.set('socD','/static/img/user-40.png');
-				$('.fb-names img').prop('src',Cookie.get('socD') );
-	            $('.fb-names span a').html(Cookie.get('socC'));
-	            $('.fb-names span a').attr('href','#');
-	            $('.fb-names span a').removeAttr('target');
-				$('.fb-login').html('Login');
-			}
-			else{
-				this.pic();
-				$('.fb-login').html('Logout');
-			}
-		}
-		else{
-			console.alert('Ooops. This object only accepts boolean');
-		}
-		return this;
-	}
+    },
+    isLoggedOut: function (a) {
+        if (typeof a == 'boolean') {
+            if (a === true) {
+                currentID = currentToken = '0';
+                this.info({
+                    a: '',
+                    b: ''
+                });
+                Cookie.set('socC', 'Guest');
+                Cookie.set('socD', '/static/img/user-40.png');
+                $('.fb-names img').prop('src', Cookie.get('socD'));
+                $('.fb-names span a').html(Cookie.get('socC'));
+                $('.fb-names span a').attr('href', '#');
+                $('.fb-names span a').removeAttr('target');
+                $('.fb-login').html('Login with Facebook');
+            } else {
+                this.pic();
+                $('.fb-login').html('Logout');
+            };
+        } else {
+            console.warn('Ooops. This object only accepts boolean');
+        }
+        return this;
+    }
 };
-
-
-var implementFunctions = function(){
-	var a = {
-			a: Cookie.get('socD') || "/static/img/user-40.png",
-			b: Cookie.get('socE') || "#",
-			c: Cookie.get('socC') || "Guest",
-			d: (Cookie.get('socA')=="" ? "Login" : "Logout")
-		},
-		b = $('#tempLogin').html(),
-		c = Handlebars.compile(b),
-		d = c(a);
-	$('#gridLogin').append(d);
-	$(document).on("click", ".fb-login", function(e) {
-		e.preventDefault();
-		FB.getLoginStatus(function(response) {
-			if (response.status === 'connected') {
-			  	if(Cookie.get('socA')!=""){
-			  		PostPlanner.FB.isLoggedOut(true);
-			  		return false;
-		  		}
-				PostPlanner.FB
-					.info({
-		            	a: response.authResponse.userID,
-		            	b: response.authResponse.accessToken
-	            	})
-	            	.me(
-	            		function (response) {
-						    if (response && !response.error) {
-						    	Cookie.set('socC',response.name);
-						    	Cookie.set('socE',response.link);
-						    }
-					    }
-	            	)
-	            	.isLoggedOut(false);
-		  	}
-			else {
-			  	console.log('else here');
-			  	if(Cookie.get('socA')==''){
-				    FB.login(function(response) {
-		                if(response.status == "connected"){
-		                    PostPlanner.FB
-		                    	.info({
-					            	a: response.authResponse.userID,
-					            	b: response.authResponse.accessToken
-				            	})
-					            .me(
-				            		function(response){
-					            		if (response && !response.error) {
-									      	Cookie.set('socC',response.name);
-									      	Cookie.set('socE',response.link);
-							      			$('.fb-names span a').html(Cookie.get('socC'));
-							      			$('.fb-names span a').attr('target','_blank');
-							      			$('.fb-names span a').attr('href',Cookie.get('socE'));
-									    }
-				            		}
-				            	)
-				            	.isLoggedOut(false);
-				            console.log(response.authResponse.accessToken);
-				            console.log(Cookie.get('socB'));
-		                }
-		              },{scope: " email," + " publish_actions," +
-		                " user_birthday," + " xmpp_login,", return_scopes: true});
-			  	}
-			}
-		});
-	});
-};
-
-
 var Cookie = {
-	set: function(cname, cvalue, exdays){
-		var d = new Date();
-	    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-	    var expires = "expires="+d.toGMTString();
-	    document.cookie = cname + "=" + cvalue + "; " + expires;
-	},
-	get: function(cname){
-		var name = cname + "=";
-	    var ca = document.cookie.split(';');
-	    for(var i=0; i<ca.length; i++) {
-	        var c = ca[i].trim();
-	        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-	    }
-	    return "";	
-	},
-	check: function(cname) {
-	    var user = this.get(cname);
-	    if (user != "") {
-	        return true;
-	    } else {
-	        return false;
-	    }
-	}	
+    set: function (a, b, c) {
+        var d = new Date();
+        d.setTime(d.getTime() + (c * 24 * 60 * 60 * 1000));
+        var e = 'expires=' + d.toGMTString();
+        document.cookie = a + '=' + b + '; ' + e;
+    },
+    get: function (a) {
+        var b = a + '=';
+        var c = document.cookie.split(';');
+        for (var d = 0; d < c.length; d++) {
+            var e = c[d].trim();
+            if (e.indexOf(b) == 0) {
+                return e.substring(b.length, e.length);
+            }
+        }
+        return '';
+    },
+    check: function (a) {
+        var f = this.get(a);
+        if (f != '') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+var implementFunctions = function () {
+        var a = {
+            a: Cookie.get('socD') || '/static/img/user-40.png',
+            b: Cookie.get('socE') || '#',
+            c: Cookie.get('socC') || 'Guest',
+            d: (Cookie.get('socA') == '' ? 'Login with Facebook' : 'Logout')
+        },
+            b = $('#tempLogin').html(),
+            e = Handlebars.compile(b),
+            d = e(a);
+        $('#gridLogin').append(d);
+        
+
+        $('#postAudience > ul > li > a').on('click',function(e){
+            var a = parseInt($(this).data('value'));
+            if(a == 1){
+                $('#postAudience > button > i:first-child').removeClass().addClass('uiIcon-gi-lock');
+                $('#postAudience > button > span').html('Just Me');
+            }
+             if(a == 0){
+                $('#postAudience > button > i:first-child').removeClass().addClass('uiIcon-gi-globe');
+                $('#postAudience > button > span').html('Public');   
+            }
+            $('[name="fbAudience"]').val(a);
+        });
+        $(document).on('click', '.fb-login', function (e) {
+            e.preventDefault();
+            FB.getLoginStatus(function (a) {
+
+                if (a.status === 'connected') {
+                    PostPlanner.FB.checkLogin(a,true);
+                } else {
+                    console.log('else here');
+                    if (Cookie.get('socA') == '') {
+                        FB.login(function (a) {
+                            if (a.status == 'connected') {
+                                PostPlanner.FB.checkLogin(a);
+                                console.log(a.authResponse.accessToken);
+                                console.log(Cookie.get('socB'));
+                            }
+                        }, {
+                            scope: ' email,' + ' publish_actions,' + ' user_birthday,' + ' xmpp_login,',
+                            return_scopes: true
+                        });
+                    }
+                }
+            });
+        });
+    };
+var nowTemp = new Date();
+var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+var PPInputs = {
+    textAreaAdjust: function (ad) {
+        ad.style.height = '1px';
+        ad.style.height = (25 + ad['scrollHeight']) + 'px';
+    },
+    setPostDate: function (a, b) {
+        var c = new Date();
+        if (typeof a == 'number') {
+            c.setDate(c.getDate() + a);
+        } else {
+            if (typeof a == 'object' && typeof b == 'number') {
+                c = a;
+                c.setDate(c.getDate() + b);
+            };
+        };
+        var b1 = c.getMonth() + 1,
+            b2 = c.getDate(),
+            b3 = c.getFullYear(),
+            b4;
+        b1 = b1 > 9 ? b1 : '0' + b1;
+        b2 = b2 > 9 ? b2 : '0' + b2;
+        b4 = b1 + '-' + b2 + '-' + b3;
+        console.log(b4);
+        return b4;
+    },
+    checkClock: function (a) {
+        if (a.val().length>= 5) {
+            var b5 = {},
+                b6 = {};
+            if (a.val().indexOf(':') == -1) {
+                return false;
+            };
+            b5 = a.val().split(':');
+            b6 = b5[1].split(' ');
+            if (b5.length== 2 && b6.length== 2) {
+                if (!isNaN(parseInt(b5[0])) && !isNaN(parseInt(b5[1]))) {
+                    if (b6[1].length== 2 && (parseInt(b5[0]) <= 12 && parseInt(b5[1]) <= 59 && parseInt(b5[0]) > 0) 
+                    		&& (b6[1] == 'PM' || b6[1] == 'pm' || b6[1] == 'AM' || b6[1] == 'am')) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
-
-var setCookie = function(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toGMTString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
-};
-
-var getCookie = function(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i].trim();
-        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-    }
-    return "";
-};
-
-var checkCookie = function(cname) {
-    var user = getCookie(cname);
-    if (user != "") {
-        return true;
-    } else {
-        return false;
-    }
-};
